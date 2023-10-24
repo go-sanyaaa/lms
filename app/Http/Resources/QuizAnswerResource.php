ab21d2773f86831da7785bea412ff8a5b74f3a57
Alexander Schetnikov <go.sanyaaa@gmail.com>
2023-10-24 13:26:35 +0300

    (!) Отображение флага ответа для пользователей

--- app/Http/Resources/QuizAnswerResource.php
+++ app/Http/Resources/QuizAnswerResource.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Resources;
 
+use App\Enums\Permissions\Roles;
 use App\Models\Quizzes\Answer;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class QuizAnswerResource extends JsonResource
 {
     public function toArray($request)
     {
+        $user = Auth::user();
+
         /** @var Answer $answer */
         $answer = $this->resource;
 
@@ -16,7 +20,7 @@ class QuizAnswerResource extends JsonResource
             'id' => $answer->id,
             'key' => (string) $answer->id,
             'text' => $answer->text,
-            'is_correct' => (boolean) $answer->is_correct,
+            'is_correct' => $user->hasRole([Roles::Auditor->value, Roles::Controller->value]) ? (boolean) $answer->is_correct : null,
         ];
     }
 }
